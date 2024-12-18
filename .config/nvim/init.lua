e32e2d90769f1a3fec1b75b05b9c909f7b02aa93
Jason Liu <jliu@Jasons-MacBook-Air.local>
2024-12-17 21:17:49 -0800

    update

---
+++ .config/nvim/init.lua
@@ -0,0 +1,3 @@
+require("init_lazy")
+require("config.vanilla")
+require("config.remap")
---
+++ .config/nvim/init_lazy.lua
@@ -0,0 +1,25 @@
+-- https://lazy.folke.io/installation
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- Make sure to setup `mapleader` and `maplocalleader` before
+-- loading lazy.nvim so that mappings are correct.
+-- This is also a good place to setup other settings (vim.opt)
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+
+-- Setup lazy.nvim
+require("lazy").setup({
+    spec = "plugins",
+    change_detection = { notify = false }
+})
