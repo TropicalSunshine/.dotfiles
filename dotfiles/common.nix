c2cd18f097fd79176dad259d4ae5f4f92734219c
jason liu <jliu@anduril.com>
2025-05-14 16:42:11 -0700

    update

--- dotfiles/common.nix
+++ dotfiles/common.nix
@@ -2,6 +2,7 @@
 {
   imports = [
     ./git.nix
+    ./nvim
   ];
 
   home.packages = with pkgs; [
--- dotfiles/nvim/default.nix
+++ dotfiles/nvim/default.nix
@@ -7,7 +7,7 @@
 
   programs.neovim = {
     enable = true;
-    extraConfig = builtins.readFile ../vim/.vimrc + ''
+    extraConfig = ''
       " LSP config (the mappings used in the default file don't quite work right)
       nnoremap <silent> gd <cmd>lua vim.lsp.buf.definition()<CR>
       nnoremap <silent> gD <cmd>lua vim.lsp.buf.declaration()<CR>
